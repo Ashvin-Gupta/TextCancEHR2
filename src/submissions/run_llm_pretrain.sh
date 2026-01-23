9098185815643fb794976a6eef6c41ac95e197e6
Ashvin-Gupta <ashvingupta00@gmail.com>
2026-01-23 13:13:56 +0000

    Changing submission parameters

--- src/submissions/run_llm_pretrain.sh
+++ src/submissions/run_llm_pretrain.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #$ -cwd                 
-#$ -pe smp 24
-#$ -l h_rt=240:0:0
-#$ -l h_vmem=7.5G
-#$ -l gpu=2
+#$ -pe smp 11
+#$ -l h_rt=1:0:0
+#$ -l h_vmem=8G
+#$ -l gpu=1
 #$ -l gpu_type=ampere
-#$ -l cluster=andrena
 #$ -j n
-#$ -o /data/home/qc25022/TextCancEHR/HPC_Pretrain/logo/
-#$ -e /data/home/qc25022/TextCancEHR/HPC_Pretrain/loge/
+#$ -o /data/home/qc25022/TextCancEHR2/HPC_Pretrain/logo/
+#$ -e /data/home/qc25022/TextCancEHR2/HPC_Pretrain/loge/
 
 set -e 
 
 # Set the base directory for your project
-BASE_DIR="/data/home/qc25022/TextCancEHR"
+BASE_DIR="/data/home/qc25022/TextCancEHR2"
 
 export WANDB_API_KEY="3256683a0a9a004cf52e04107a3071099a53038e"
 
@@ -28,8 +27,8 @@ cd "${BASE_DIR}"
 echo "Starting experiment from directory: $(pwd)"
 export PYTHONPATH="${BASE_DIR}:${PYTHONPATH}"
 
-# python -m src.pipelines.llm_pretrain.py --config_filepath src/configs/llm_pretrain.yaml 
-torchrun --nproc_per_node=2 src/pipelines/llm_pretrain.py --config_filepath src/configs/llm_pretrain.yaml
+python -m src.pipelines.llm_pretrain.py --config_filepath src/configs/llm_pretrain.yaml 
+# torchrun --nproc_per_node=2 src/pipelines/llm_pretrain.py --config_filepath src/configs/llm_pretrain.yaml
 
 echo "Pipeline finished."
 deactivate
