612b08eca15d028ac0be9854f765c831a6761461
Ashvin-Gupta <ashvingupta00@gmail.com>
2026-01-26 11:46:21 +0000

    Changing scripts to do pretraining

--- src/data/unified_dataset.py
+++ src/data/unified_dataset.py
@@ -30,7 +30,7 @@ class UnifiedEHRDataset(Dataset):
         self.data_type = data_type
         # Load the patient records from the .pkl files for the specified split
         # self.patient_records = self._load_data(data_dir, split)
-        if split == 'tuning' or split == 'held_out' or split == 'training':
+        if split == 'tuning' or split == 'held_out':
             self.patient_records = self._load_data(data_dir, split, limit=1)
         else:
             # Chaning to 5 to see result and inference
