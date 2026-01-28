02dbf97d5caa948e6fbe8e1a0ae9eca82af6a983
Ashvin-Gupta <ashvingupta00@gmail.com>
2026-01-28 11:21:43 +0000

    Debugging pretrain

--- src/submissions/pretrain.sh
+++ src/submissions/pretrain.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #$ -cwd                 
-#$ -pe smp 24
-#$ -l h_rt=24:0:0
-#$ -l h_vmem=7.5G
-#$ -l gpu=2
+#$ -pe smp 8
+#$ -l h_rt=1:0:0
+#$ -l h_vmem=11G
+#$ -l gpu=1
 #$ -l gpu_type=ampere
-#$ -l cluster=andrena
 #$ -j n
 #$ -o /data/home/qc25022/TextCancEHR2/HPC_Pretrain/logo/
 #$ -e /data/home/qc25022/TextCancEHR2/HPC_Pretrain/loge/
@@ -28,8 +27,8 @@ cd "${BASE_DIR}"
 echo "Starting experiment from directory: $(pwd)"
 export PYTHONPATH="${BASE_DIR}:${PYTHONPATH}"
 
-# python -m src.pipelines.llm_pretrain --config_filepath src/configs/llm_pretrain.yaml 
-torchrun --nproc_per_node=2 src/pipelines/llm_pretrain.py --config_filepath src/configs/llm_pretrain.yaml
+python -m src.pipelines.llm_pretrain --config_filepath src/configs/llm_pretrain.yaml 
+# torchrun --nproc_per_node=2 src/pipelines/llm_pretrain.py --config_filepath src/configs/llm_pretrain.yaml
 
 echo "Pipeline finished."
 deactivate
